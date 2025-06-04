f2c4b9362cacde212c45d9a862b18cc7707c0917
zakhar <zakhar.savchyn.23@pnu.edu.ua>
2025-06-04 21:40:05 +0300

    change main method in Dispatcher to use ExecutorService instead of simple Thread

--- src/Airport/Dispatcher.java
+++ src/Airport/Dispatcher.java
@@ -1,24 +1,29 @@
 package Airport;
 
 import java.util.*;
+import java.util.concurrent.*;
 
 public class Dispatcher {
     public static void main(String[] args) {
-        List<Plane> planes = Controller.createPlanes(100);
+        List<Plane> planes = Controller.createPlanes(1000);
 
-        List<Thread> threads = new ArrayList<>();
+        int numThreads = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(numThreads);
         for (Plane plane : planes) {
-            Thread thread = new Thread(plane);
-            threads.add(thread);
-            thread.start();
+            executorService.submit(plane);
         }
 
-        for (Thread thread : threads) {
-            try {
-                thread.join();
-            } catch (InterruptedException e) {
-                System.err.println("Thread was interrupted while waiting: " + e.getMessage());
+        executorService.shutdown();
+        try {
+            if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                executorService.shutdownNow();
+                if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                    System.err.println("Pool did not terminate");
+                }
             }
+        } catch (InterruptedException ie) {
+            executorService.shutdownNow();
+            Thread.currentThread().interrupt();
         }
 
         List<Bus> buses = Controller.getAllBuses();
